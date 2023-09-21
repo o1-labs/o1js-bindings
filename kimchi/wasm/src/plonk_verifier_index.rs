a5a0ee4ce3f1d439740efba79c2d93e4c605f9bd
Danny Willems <be.danny.willems@gmail.com>
2023-09-21 12:50:03 +0200

    DlogVerifierIndex: reorganize struct fields
    Stay in line with struct in proof-systems. Ease reading.

--- kimchi/wasm/src/plonk_verifier_index.rs
+++ kimchi/wasm/src/plonk_verifier_index.rs
@@ -750,34 +750,30 @@ macro_rules! impl_verification_key {
                 let index =
                     DlogVerifierIndex {
                         domain,
-
+                        max_poly_size: max_poly_size as usize,
+                        srs: {
+                            let res = once_cell::sync::OnceCell::new();
+                            res.set(srs.0.clone()).unwrap();
+                            res
+                        },
+                        public: public_ as usize,
+                        prev_challenges: prev_challenges as usize,
                         sigma_comm: array_init(|i| (&evals.sigma_comm[i]).into()),
-                        generic_comm: (&evals.generic_comm).into(),
                         coefficients_comm: array_init(|i| (&evals.coefficients_comm[i]).into()),
+                        generic_comm: (&evals.generic_comm).into(),
 
                         psm_comm: (&evals.psm_comm).into(),
 
                         complete_add_comm: (&evals.complete_add_comm).into(),
                         mul_comm: (&evals.mul_comm).into(),
                         emul_comm: (&evals.emul_comm).into(),
-
                         endomul_scalar_comm: (&evals.endomul_scalar_comm).into(),
-                        xor_comm: (&evals.xor_comm).as_ref().map(Into::into),
                         range_check0_comm: (&evals.range_check0_comm).as_ref().map(Into::into),
                         range_check1_comm: (&evals.range_check1_comm).as_ref().map(Into::into),
+                        xor_comm: (&evals.xor_comm).as_ref().map(Into::into),
+                        rot_comm: (&evals.rot_comm).as_ref().map(Into::into),
                         foreign_field_add_comm: (&evals.foreign_field_add_comm).as_ref().map(Into::into),
                         foreign_field_mul_comm: (&evals.foreign_field_mul_comm).as_ref().map(Into::into),
-                        rot_comm: (&evals.rot_comm).as_ref().map(Into::into),
-
-                        w: {
-                            let res = once_cell::sync::OnceCell::new();
-                            res.set(zk_w(domain, 3)).unwrap();
-                            res
-                        },
-                        endo: endo_q,
-                        max_poly_size: max_poly_size as usize,
-                        public: public_ as usize,
-                        prev_challenges: prev_challenges as usize,
                         permutation_vanishing_polynomial_m: {
                             let res = once_cell::sync::OnceCell::new();
                             res.set(permutation_vanishing_polynomial(domain, 3)).unwrap();
@@ -792,17 +788,18 @@ macro_rules! impl_verification_key {
                             shifts.s5.into(),
                             shifts.s6.into()
                         ],
-                        srs: {
-                            let res = once_cell::sync::OnceCell::new();
-                            res.set(srs.0.clone()).unwrap();
-                            res
-                        },
-
+                        // TODO(dw): zkpm?
                         zk_rows: 3,
 
+                        w: {
+                            let res = once_cell::sync::OnceCell::new();
+                            res.set(zk_w3(domain)).unwrap();
+                            res
+                        },
+                        endo: endo_q,
+                        lookup_index: lookup_index.map(Into::into),
                         linearization,
                         powers_of_alpha,
-                        lookup_index: lookup_index.map(Into::into),
                     };
                 (index, srs.0.clone())
             }
