8cd4f8f9015ed9c2eca6b1e8dab2b8b87c25b6bb
Mikhail Volkhov <volhovm.cs@gmail.com>
2023-09-26 21:02:35 +0100

    Rename function, see mina#14205 and mina#14206

--- ocaml/lib/pickles_bindings.ml
+++ ocaml/lib/pickles_bindings.ml
@@ -465,7 +465,7 @@ let pickles_compile (choices : pickles_rule_js array)
   let public_output_size = signature##.publicOutputSize in
   let override_wrap_domain =
     Js.Optdef.to_option signature##.overrideWrapDomain
-    |> Option.map ~f:Pickles_base.Proofs_verified.of_int
+    |> Option.map ~f:Pickles_base.Proofs_verified.of_int_exn
   in
   let (Choices choices) =
     Choices.of_js ~public_input_size ~public_output_size choices
