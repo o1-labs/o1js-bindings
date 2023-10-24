a10a725bc71deb9d8617e64f1254ceca532f22e0
Gregor <gregor.mitscha-baude@gmx.at>
2023-10-24 12:29:20 +0200

    tweak script

--- scripts/update-wasm-types.sh
+++ scripts/update-wasm-types.sh
@@ -5,6 +5,7 @@ set -e
 SNARKY_JS_PATH="src/lib/snarkyjs"
 DIR_PATH=$(dirname "$0")
 KIMCHI_BINDINGS="$SNARKY_JS_PATH/src/bindings/kimchi"
+_NODE_BINDINGS="$SNARKY_JS_PATH/src/bindings/compiled/_node_bindings"
 NODE_BINDINGS="$SNARKY_JS_PATH/src/bindings/compiled/node_bindings"
 
 export DUNE_USE_DEFAULT_LINKER="y"
@@ -14,12 +15,14 @@ export DUNE_USE_DEFAULT_LINKER="y"
 pushd ../../..
   dune b $KIMCHI_BINDINGS/js/node_js
 
-  chmod -R 777 "$NODE_BINDINGS"
+  chmod 777 "$_NODE_BINDINGS"/*
 
-  cp _build/default/$KIMCHI_BINDINGS/js/node_js/plonk_wasm* "$NODE_BINDINGS"/
-  mv -f $NODE_BINDINGS/plonk_wasm.js $NODE_BINDINGS/plonk_wasm.cjs
-  mv -f $NODE_BINDINGS/plonk_wasm.d.ts $NODE_BINDINGS/plonk_wasm.d.cts
+  cp _build/default/$KIMCHI_BINDINGS/js/node_js/plonk_wasm* "$_NODE_BINDINGS"/
+  mv -f $_NODE_BINDINGS/plonk_wasm.js $_NODE_BINDINGS/plonk_wasm.cjs
+  mv -f $_NODE_BINDINGS/plonk_wasm.d.ts $_NODE_BINDINGS/plonk_wasm.d.cts
 
-  chmod 777 "$NODE_BINDINGS"/*
-  node "$SNARKY_JS_PATH/src/build/fix-wasm-bindings-node.js" "$NODE_BINDINGS/plonk_wasm.cjs"
+  chmod 777 "$_NODE_BINDINGS"/*
+  node "$SNARKY_JS_PATH/src/build/fix-wasm-bindings-node.js" "$_NODE_BINDINGS/plonk_wasm.cjs"
+
+  cp $_NODE_BINDINGS/plonk_wasm*d.{cts,ts} $NODE_BINDINGS/
 popd
