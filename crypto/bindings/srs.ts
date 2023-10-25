edefd675062db3dff6cdfc4932a5cdc5a578b6a7
Gregor <gregor.mitscha-baude@gmx.at>
2023-10-25 13:40:41 +0200

    implement srs caching

--- crypto/bindings/srs.ts
+++ crypto/bindings/srs.ts
@@ -13,9 +13,9 @@ export { srs, setSrsCache, unsetSrsCache };
 
 type WasmSrs = WasmFpSrs | WasmFqSrs;
 
-type Srs = Record<number, WasmSrs>;
+type SrsStore = Record<number, WasmSrs>;
 
-function empty(): Srs {
+function empty(): SrsStore {
   return {};
 }
 
@@ -46,6 +46,10 @@ function srs(wasm: Wasm, conversion: RustConversion) {
 
 function srsPerField(f: 'fp' | 'fq', wasm: Wasm, conversion: RustConversion) {
   // note: these functions are properly typed, thanks to TS template literal types
+  let createSrs = wasm[`caml_${f}_srs_create_parallel`];
+  let getSrs = wasm[`caml_${f}_srs_get`];
+  let setSrs = wasm[`caml_${f}_srs_set`];
+
   let maybeLagrangeCommitment = wasm[`caml_${f}_srs_maybe_lagrange_commitment`];
   let lagrangeCommitment = wasm[`caml_${f}_srs_lagrange_commitment`];
   let setLagrangeBasis = wasm[`caml_${f}_srs_set_lagrange_basis`];
@@ -56,11 +60,49 @@ function srsPerField(f: 'fp' | 'fq', wasm: Wasm, conversion: RustConversion) {
      * returns existing stored SRS or falls back to creating a new one
      */
     create(size: number): WasmSrs {
-      // TODO caching
-      let srs = (srsStore[f][size] ??=
-        wasm[`caml_${f}_srs_create_parallel`](size));
+      let srs = srsStore[f][size] as WasmSrs | undefined;
+
+      if (srs === undefined) {
+        if (cache === undefined) {
+          // if there is no cache, create SRS in memory
+          srs = createSrs(size);
+        } else {
+          let key = cacheKeySrs(f, size);
+
+          // try to read SRS from cache / recompute and write if not found
+          try {
+            let bytes = cache.read(key, 'string');
+
+            // TODO: this takes a bit to long, about 300ms for 2^16
+            // `pointsToRust` is the clear bottleneck
+            let jsonSrs: OrInfinityJson[] = JSON.parse(
+              new TextDecoder().decode(bytes)
+            );
+            let mlSrs = MlArray.mapTo(jsonSrs, OrInfinity.fromJSON);
+            let wasmSrs = conversion[f].pointsToRust(mlSrs);
+
+            srs = setSrs(wasmSrs);
+          } catch (e) {
+            // not in cache
+            // TODO we need `canWrite` boolean on cache to avoid expensive write preparation
+            srs = createSrs(size);
+            let wasmSrs = getSrs(srs);
+
+            let mlSrs = conversion[f].pointsFromRust(wasmSrs);
+            let jsonSrs = MlArray.mapFrom(mlSrs, OrInfinity.toJSON);
+            let bytes = new TextEncoder().encode(JSON.stringify(jsonSrs));
+
+            try {
+              cache.write(key, bytes, 'string');
+            } catch (e) {}
+          }
+        }
+
+        srsStore[f][size] = srs;
+      }
+
       // should we do call freeOnFinalize() and expose a function to clean the SRS cache?
-      return srs;
+      return srsStore[f][size];
     },
 
     /**
