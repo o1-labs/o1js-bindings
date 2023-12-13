7cffd8b827d6b7f217627d996367f615db4b6476
Martin Minkov <minkovlmartin@gmail.com>
2023-12-13 09:52:39 -0800

    feat: fix scripts for o1js building

--- scripts/update-wasm-and-types.sh
+++ scripts/update-wasm-and-types.sh
@@ -5,27 +5,24 @@
 
 set -e
 
-SNARKY_JS_PATH="src/lib/snarkyjs"
-DIR_PATH=$(dirname "$0")
-KIMCHI_BINDINGS="$SNARKY_JS_PATH/src/bindings/kimchi"
+SNARKY_JS_PATH="."
+MINA_PATH="$SNARKY_JS_PATH/src/mina"
+KIMCHI_BINDINGS="$MINA_PATH/src/lib/crypto/kimchi"
 _NODE_BINDINGS="$SNARKY_JS_PATH/src/bindings/compiled/_node_bindings"
 NODE_BINDINGS="$SNARKY_JS_PATH/src/bindings/compiled/node_bindings"
 
 export DUNE_USE_DEFAULT_LINKER="y"
 
 # change to mina directory
+dune b $KIMCHI_BINDINGS/js/node_js
 
-pushd ../../..
-  dune b $KIMCHI_BINDINGS/js/node_js
+chmod 777 "$_NODE_BINDINGS"/*
 
-  chmod 777 "$_NODE_BINDINGS"/*
+cp _build/default/$KIMCHI_BINDINGS/js/node_js/plonk_wasm* "$_NODE_BINDINGS"/
+mv -f $_NODE_BINDINGS/plonk_wasm.js $_NODE_BINDINGS/plonk_wasm.cjs
+mv -f $_NODE_BINDINGS/plonk_wasm.d.ts $_NODE_BINDINGS/plonk_wasm.d.cts
 
-  cp _build/default/$KIMCHI_BINDINGS/js/node_js/plonk_wasm* "$_NODE_BINDINGS"/
-  mv -f $_NODE_BINDINGS/plonk_wasm.js $_NODE_BINDINGS/plonk_wasm.cjs
-  mv -f $_NODE_BINDINGS/plonk_wasm.d.ts $_NODE_BINDINGS/plonk_wasm.d.cts
+chmod 777 "$_NODE_BINDINGS"/*
+node "$SNARKY_JS_PATH/src/build/fix-wasm-bindings-node.js" "$_NODE_BINDINGS/plonk_wasm.cjs"
 
-  chmod 777 "$_NODE_BINDINGS"/*
-  node "$SNARKY_JS_PATH/src/build/fix-wasm-bindings-node.js" "$_NODE_BINDINGS/plonk_wasm.cjs"
-
-  cp $_NODE_BINDINGS/plonk_wasm.d.cts $NODE_BINDINGS/
-popd
+cp $_NODE_BINDINGS/plonk_wasm.d.cts $NODE_BINDINGS/
