ba9b30230bc1add0c8b1a8d32edd5f4adcfb665f
Martin Minkov <minkovlmartin@gmail.com>
2024-01-02 15:14:44 -0800

    Update getSnarky function to getO1js

--- js/wrapper.d.ts
+++ js/wrapper.d.ts
@@ -1,11 +1,11 @@
 import * as wasm from '../compiled/node_bindings/plonk_wasm.cjs';
 
-export { WasmModule, getWasm, getSnarky, withThreadPool };
+export { WasmModule, getWasm, getO1js, withThreadPool };
 
 type WasmModule = typeof wasm;
 
 declare function getWasm(): WasmModule;
 
-declare function getSnarky(): any;
+declare function getO1js(): any;
 
 declare function withThreadPool<T>(run: () => Promise<T>): Promise<T>;
--- js/wrapper.js
+++ js/wrapper.js
@@ -1,18 +1,18 @@
 import { wasm, withThreadPool } from './node/node-backend.js';
 
-let snarky;
+let o1js;
 
 // this dynamic import makes jest respect the import order
 // otherwise the cjs file gets imported before its implicit esm dependencies and fails
 CJS: if (typeof require !== 'undefined') {
-  snarky = require('../compiled/_node_bindings/o1js_node.bc.cjs');
+  o1js = require('../compiled/_node_bindings/o1js_node.bc.cjs');
 }
-ESM: snarky = (await import('../compiled/_node_bindings/o1js_node.bc.cjs'))
+ESM: o1js = (await import('../compiled/_node_bindings/o1js_node.bc.cjs'))
   .default;
 
-export { getSnarky, getWasm, withThreadPool };
+export { getO1js, getWasm, withThreadPool };
 
-let getSnarky = () => snarky;
+let getO1js = () => o1js;
 
 function getWasm() {
   return wasm;
