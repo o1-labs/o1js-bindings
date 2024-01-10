f5eb6c12edce016c5e0fe4334f4e716e0c893957
gabrielbosio <gabrielbosio95@gmail.com>
2024-01-10 22:54:33 +0100

    Add bn254 SRS wasm bindings

--- kimchi/js/bindings.js
+++ kimchi/js/bindings.js
@@ -277,6 +277,98 @@ function caml_fq_srs_add_lagrange_basis(srs, log2_size) {
   return plonk_wasm.caml_fq_srs_add_lagrange_basis(srs, log2_size);
 }
 
+// Provides: caml_bn254_fp_srs_create
+// Requires: plonk_wasm, free_on_finalize
+var caml_bn254_fp_srs_create = function (i) {
+  return free_on_finalize(plonk_wasm.caml_bn254_fp_srs_create(i));
+};
+
+// Provides: caml_bn254_fp_srs_write
+// Requires: plonk_wasm, caml_jsstring_of_string
+var caml_bn254_fp_srs_write = function (append, t, path) {
+  if (append === 0) {
+    append = undefined;
+  } else {
+    append = append[1];
+  }
+  return plonk_wasm.caml_bn254_fp_srs_write(append, t, caml_jsstring_of_string(path));
+};
+
+// Provides: caml_bn254_fp_srs_read
+// Requires: plonk_wasm, caml_jsstring_of_string
+var caml_bn254_fp_srs_read = function (offset, path) {
+  if (offset === 0) {
+    offset = undefined;
+  } else {
+    offset = offset[1];
+  }
+  var res = plonk_wasm.caml_bn254_fp_srs_read(offset, caml_jsstring_of_string(path));
+  if (res) {
+    return [0, res]; // Some(res)
+  } else {
+    return 0; // None
+  }
+};
+
+// Provides: caml_bn254_fp_srs_lagrange_commitment
+// Requires: plonk_wasm, tsRustConversion
+var caml_bn254_fp_srs_lagrange_commitment = function (t, domain_size, i) {
+  var res = plonk_wasm.caml_bn254_fp_srs_lagrange_commitment(t, domain_size, i);
+  return tsRustConversion.bn254Fp.polyCommFromRust(res);
+};
+
+// Provides: caml_bn254_fp_srs_commit_evaluations
+// Requires: plonk_wasm, tsRustConversion
+var caml_bn254_fp_srs_commit_evaluations = function (t, domain_size, fps) {
+  var res = plonk_wasm.caml_bn254_fp_srs_commit_evaluations(
+    t,
+    domain_size,
+    tsRustConversion.bn254Fp.vectorToRust(fps)
+  );
+  return tsRustConversion.bn254Fp.polyCommFromRust(res);
+};
+
+// Provides: caml_bn254_fp_srs_b_poly_commitment
+// Requires: plonk_wasm, tsRustConversion
+var caml_bn254_fp_srs_b_poly_commitment = function (srs, chals) {
+  var res = plonk_wasm.caml_bn254_fp_srs_b_poly_commitment(
+    srs,
+    tsRustConversion.fieldsToRustFlat(chals)
+  );
+  return tsRustConversion.bn254Fp.polyCommFromRust(res);
+};
+
+// Provides: caml_bn254_fp_srs_batch_accumulator_check
+// Requires: plonk_wasm, tsRustConversion
+var caml_bn254_fp_srs_batch_accumulator_check = function (srs, comms, chals) {
+  var rust_comms = tsRustConversion.bn254Fp.pointsToRust(comms);
+  var rust_chals = tsRustConversion.bn254Fp.vectorToRust(chals);
+  var ok = plonk_wasm.caml_bn254_fp_srs_batch_accumulator_check(
+    srs,
+    rust_comms,
+    rust_chals
+  );
+  return ok;
+};
+
+// Provides: caml_bn254_fp_srs_batch_accumulator_generate
+// Requires: plonk_wasm, tsRustConversion
+var caml_bn254_fp_srs_batch_accumulator_generate = function (srs, n_comms, chals) {
+  var rust_chals = tsRustConversion.bn254Fp.vectorToRust(chals);
+  var rust_comms = plonk_wasm.caml_bn254_fp_srs_batch_accumulator_generate(
+    srs,
+    n_comms,
+    rust_chals
+  );
+  return tsRustConversion.bn254Fp.pointsFromRust(rust_comms);
+};
+
+// Provides: caml_bn254_fp_srs_h
+// Requires: plonk_wasm, tsRustConversion
+var caml_bn254_fp_srs_h = function (t) {
+  return tsRustConversion.bn254Fp.pointFromRust(plonk_wasm.caml_bn254_fp_srs_h(t));
+};
+
 // gate vector - pasta fp
 
 // Provides: caml_pasta_fp_plonk_gate_vector_create
