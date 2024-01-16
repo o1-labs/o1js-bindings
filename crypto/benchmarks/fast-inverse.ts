76d879c512600ae0d132cc474ffd9cd327577614
Gregor <gregor.mitscha-baude@gmx.at>
2024-01-16 15:42:46 +0100

    further slight improvements

--- crypto/benchmarks/fast-inverse.ts
+++ crypto/benchmarks/fast-inverse.ts
@@ -4,13 +4,14 @@ import { mod, inverse } from '../finite_field.js';
 
 export { createFastInverse };
 
-const w = 31n;
-const wMask = (1n << w) - 1n;
+const w = 31;
+const wn = BigInt(w);
+const wMask = (1n << wn) - 1n;
 const hiBits = 31;
 
 function createFastInverse(p: bigint) {
   const n = Math.ceil(255 / Number(w));
-  const kmax = 2n * BigInt(n) * w;
+  const kmax = 2n * BigInt(n * w);
 
   // precomputed constant for correcting 2^k/x -> 1/x, by multiplying with 2^-kmax * 2^(kmax - k)
   const twoToMinusKmax = inverse(1n << kmax, p)!;
@@ -84,24 +85,21 @@ function fastInverse(
 
     let unew = u * f0n - v * g0n;
     let vnew = v * g1n - u * f1n;
-    u = unew >> w;
-    v = vnew >> w;
+    u = unew >> wn;
+    v = vnew >> wn;
 
-    if (u < 0) {
-      [u, f0n, g0n] = [-u, -f0n, -g0n];
-    }
-    if (v < 0) {
-      [v, f1n, g1n] = [-v, -f1n, -g1n];
-    }
+    if (u < 0) (u = -u), (f0n = -f0n), (g0n = -g0n);
+    if (v < 0) (v = -v), (f1n = -f1n), (g1n = -g1n);
+
     let rnew = r * f0n + s * g0n;
-    let snew = r * f1n + s * g1n;
+    let snew = s * g1n + r * f1n;
     r = rnew;
     s = snew;
 
     // these assertions are all true, enable when debugging:
     // let lin = v * r + u * s;
-    // let k = BigInt(i + 1) * w;
     // assert(lin === p || lin === -p, 'linear combination');
+    // let k = BigInt((i + 1) * w);
     // assert(mod(x * r + u * 2n ** k, p) === 0n, 'mod p, r');
     // assert(mod(x * s - v * 2n ** k, p) === 0n, 'mod p, s');
 
@@ -109,11 +107,12 @@ function fastInverse(
 
     // empirically this never happens, but there might be unlucky edge cases where it does, due to sign flips
     if (v === 0n) {
+      assert(u === 1n, 'u = 1');
       s = mod(-r, p);
       break;
     }
   }
-  let k = BigInt(i + 1) * w;
+  let k = BigInt((i + 1) * w);
 
   // now s = 2^k/x mod p
   // correction step to go from 2^k/x to 1/x
@@ -121,7 +120,7 @@ function fastInverse(
   s = mod(s * (1n << (kmax - k)), p); // s <- s * 2^(kmax - k) = 1/x
 
   // yes this has a slight cost and the assert is never triggered,
-  // but it's worth it for the sake of safety
+  // but it's worth having for the sake of assurance
   assert(mod(x * s - 1n, p) === 0n, 'mod p');
   return s;
 }
