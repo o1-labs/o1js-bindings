d88a666e925aa3da8cf35d6a19533cae610637eb
Gregor <gregor.mitscha-baude@gmx.at>
2024-03-12 22:07:03 +0100

    collapse wrapper.js and snarky.js

--- js/wrapper.d.ts
+++
@@ -1,9 +0,0 @@
-import * as wasm from '../compiled/node_bindings/plonk_wasm.cjs';
-
-export { WasmModule, wasm, snarky, withThreadPool };
-
-type WasmModule = typeof wasm;
-
-declare let snarky: any;
-
-declare function withThreadPool<T>(run: () => Promise<T>): Promise<T>;
--- js/wrapper.web.js
+++
@@ -1,8 +0,0 @@
-import { initO1, withThreadPool } from './web/web-backend.js';
-
-export { snarky, wasm, withThreadPool };
-
-await initO1();
-
-let snarky = globalThis.__snarky;
-let wasm = globalThis.plonk_wasm;
