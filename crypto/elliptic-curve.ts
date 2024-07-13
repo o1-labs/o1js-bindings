e2d16003a7987f1c5a7fb899600200bae5d27736
ymekuria <ymekuria@gmail.com>
2024-07-13 01:41:27 -0700

    feat(elliptic-curve.ts): add isPointAtInfinity typeguard to projectiveFromAffine function

--- crypto/elliptic-curve.ts
+++ crypto/elliptic-curve.ts
@@ -296,13 +296,9 @@ function projectiveScale(g: GroupProjective, x: bigint | boolean[], p: bigint) {
   return h;
 }
 
-function projectiveFromAffine({
-  x,
-  y,
-  infinity,
-}: GroupAffine): GroupProjective {
-  if (infinity) return projectiveZero;
-  return { x, y, z: 1n };
+function projectiveFromAffine(point: GroupAffine): GroupProjective {
+  if (isPointAtInfinity(point)) return projectiveZero;
+  return { x: point.x, y: point.y, z: 1n };
 }
 
 function projectiveToAffine(g: GroupProjective, p: bigint): GroupAffine {
