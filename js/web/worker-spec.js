50e264fd5dae801499a5da7dd99d3aa2dcd5b80f
Florian <florian@technotro.com>
2025-01-27 11:22:36 +0100

    comment

--- js/web/worker-spec.js
+++ js/web/worker-spec.js
@@ -171,11 +171,11 @@ function workerSpec(wasm) {
     },
     caml_fp_srs_lagrange_commitments_whole_domain: {
       args: [wasm.WasmFpSrs, undefined /* number */],
-      res: undefined /* number */,
+      res: undefined /* number, ptr */,
     },
     caml_fq_srs_lagrange_commitments_whole_domain: {
       args: [wasm.WasmFqSrs, undefined /* number */],
-      res: undefined /* number */,
+      res: undefined /* number, ptr */,
     },
   };
 }
