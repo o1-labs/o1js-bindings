eea0201785b6d229c93d0f73147808249a6c5372
Florian <florian@technotro.com>
2025-02-18 18:26:55 +0700

    fix import

--- mina-transaction/v2/index.ts
+++ mina-transaction/v2/index.ts
@@ -1,4 +1,4 @@
 export * as Leaves from './leaves.js';
 export { BindingsType as Type } from './type.js';
 export * from './util.js';
-export * as Layout from '../gen/js-layout-v2.js';
+export * as Layout from '../gen/v2/js-layout.js';
