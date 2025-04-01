fa8a2eaa84e5ac2e1fc1fd454fabbd355c330cbd
Danny Willems <be.danny.willems@gmail.com>
2025-04-01 19:38:07 +0200

    scripts/check-commit: make shellcheck happy

--- scripts/check-commit.sh
+++ scripts/check-commit.sh
@@ -6,12 +6,12 @@ MINA_PATH="src/mina"
 
 BUILT=$(cat src/bindings/MINA_COMMIT | sed 's/.* //')
 
-pushd "$MINA_PATH"
+pushd "${MINA_PATH}"
   MINA_COMMIT=$(git rev-parse HEAD)
 popd
 
-if [ "$BUILT" != "$MINA_COMMIT" ]
+if [ "${BUILT}" != "${MINA_COMMIT}" ]
 then
-  echo mina is on $MINA_COMMIT but bindings were built for $BUILT
+  echo mina is on "${MINA_COMMIT}" but bindings were built for "${BUILT}"
   exit 1
 fi
